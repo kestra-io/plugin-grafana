01078f44bc1b6a5cdc5df4e02f2460ac695fa92f
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-27 12:13:38 +0530

    docs: add missing package-info description

--- src/main/java/io/kestra/plugin/grafana/loki/package-info.java
+++ src/main/java/io/kestra/plugin/grafana/loki/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     title = "Grafana-loki",
-    description = "",
+    description = "This sub-group contains tasks and models for interacting with Grafana Loki",
     categories = { PluginSubGroup.PluginCategory.STORAGE, PluginSubGroup.PluginCategory.TOOL }
 )
 package io.kestra.plugin.grafana.loki;
